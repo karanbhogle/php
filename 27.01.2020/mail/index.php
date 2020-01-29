13a9b3dc1495a5188440664b21c8e81303e1fd95
Karan Bhogle <karanbhogle.glsica15@gmail.com>
2020-01-29 10:39:36 +0530

    sending mail with attachment completed using PHPMailer

--- 27.01.2020/mail/index.php
+++ 27.01.2020/mail/index.php
@@ -1,29 +1,92 @@
 <?php
-require_once("PHPMailer/PHPMailerAutoload.php");
-$mailer = new PHPMailer();
-$mailer->IsSMTP();
-$mailer->Host = 'ssl://smtp.gmail.com';
-$mailer->Port = 587; //can be 587
-$mailer->SMTPAuth = TRUE;
-// Change this to your gmail address
-$mailer->Username = 'karanbhoglenow@gmail.com';  
-// Change this to your gmail password
-$mailer->Password = 'ke54628epReal8171ms';  
-// Change this to your gmail address
-$mailer->From = 'kevinshah0697@gmail.com';  
-// This will reflect as from name in the email to be sent
-$mailer->FromName = 'Admin Chu Admin'; 
-$mailer->Body = 'This is the body of your email.';
-$mailer->Subject = 'This is your subject.';
-// This is where you want your email to be sent
-$mailer->AddAddress('kevinshah0697@gmail.com');  
-if(!$mailer->Send())
-{
-    echo "Message was not sent<br/ >";
-    echo "Mailer Error: " . $mailer->ErrorInfo;
+require 'PHPMailer/PHPMailerAutoload.php';
+
+if(isset($_POST['btnSubmit'])){
+    $to = $_POST['textTo'];
+    $subject = $_POST['textSubject'];
+    $attachment = $_FILES['fileAttachment'];
+    $body = $_POST['textareaBody'];
+    $file_tmp  = $_FILES['fileAttachment']['tmp_name'];
+    $file_name = $_FILES['fileAttachment']['name'];
+
+    if(!empty($to)){
+        $mail = new PHPMailer;
+
+        $mail->IsSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';                 // Specify main and backup server
+        $mail->Port = 587;                                    // Set the SMTP port
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'karanbhoglenow@gmail.com';                // SMTP username
+        $mail->Password = 'ke54628epReal8171ms';                  // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+        $mail->From = 'karanbhoglenow@gmail.com';
+        $mail->FromName = 'Admin Chhu Admin';
+        $mail->AddAddress($to);  // Add a recipient
+        // $mail->AddAddress('ellen@example.com');               // Name is optional
+
+        $mail->IsHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        move_uploaded_file($file_tmp,"uploads/".$file_name);
+        $mail->addAttachment("uploads/".$file_name);
+        
+        if(!$mail->Send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+            exit;
+        }
+        else{
+            echo 'Message has been sent';
+        }
+    }
+    else{
+        echo 'Please add a recepient';
+    }
 }
-else
-{
-    echo "Message has been sent";
-}
+
 ?>
+
+
+
+
+<head>
+    <style>
+        .col-25{
+            float:left;
+            width:25%;
+        }
+        .col-75{
+            float:left;
+            width:75%;
+        }
+    </style>
+</head>
+<form action="index.php" method="POST" enctype="multipart/form-data">
+<div>
+    <div>
+        <div class="col-25"><label>To:</label></div>
+        <div class="col-75"><input type="text" name="textTo"></div>
+    </div>
+    <div>
+        <div class="col-25"><label>Add Attachment:</label></div>
+        <div class="col-75"><input type="file" name="fileAttachment"></div>
+    </div>
+
+    <div>
+        <div class="col-25"><label>Subject:</label></div>
+        <div class="col-75"><input type="text" name="textSubject"></div>
+    </div>
+
+    <div>
+        <div class="col-25"><label>Body:</label></div>
+        <div class="col-75"><textarea rows="10" cols="60" name="textareaBody"></textarea></div>
+    </div>
+    
+    <div>
+        <div><input type="submit" value="Send Mail" name="btnSubmit"></div>
+    </div>
+</div>    
+</form>
